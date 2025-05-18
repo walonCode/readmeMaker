bb7714f937ce8d27bfd0fb06310c5320f678b686
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-05-18 01:31:39 +0000

    [Update] Add files map to be ignore during the tree parsing function.

--- internal/tree/buildTree.go
+++ internal/tree/buildTree.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 )
 
+var ignoredFolders = map[string]bool{
+	"node_modules":true,
+	".git" : true,
+	"dist": true,
+	".next" : true,
+}
+
 type TreeBuilder struct {
 	Builder     strings.Builder
 	FileCount   int
@@ -26,6 +33,10 @@ func (tb *TreeBuilder)WalkAndCount() error{
 			return nil
 		}
 
+		if d.IsDir() && ignoredFolders[d.Name()] {
+			return filepath.SkipDir
+		}
+		
 		relPath,_ := filepath.Rel(tb.Root, path)
 		depth := strings.Count(relPath, string(os.PathSeparator))
 		prefix := strings.Repeat("|  ", depth)
